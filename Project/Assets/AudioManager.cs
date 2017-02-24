a364d689f0f6dea177101534237883c31a4e5c2e
Brice Puls <briceepuls@gmail.com>
2017-02-23 18:57:39 -0600

    Clean up terrain generator, begin audio manager

---
+++ Project/Assets/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public enum TrackTypes
+	{
+		Bass,
+		Clav,
+		Keys,
+		Pizz,
+		Drums
+	}
+
+
+}
