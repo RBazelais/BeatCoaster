f50b96ff8b8a0be426b200e768acf6d7d04e4d94
Catt Small <cattsmall@gmail.com>
2017-02-25 13:09:48 -0700

    added game over info text and edited main menu

---
+++ Project/Assets/Scripts/UI/GameOverInfoText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOverListenersText : MonoBehaviour {
+	[SerializeField] private TextMeshProUGUI text;
+
+	private void OnEnable() {
+		PersonManager.instance.SignalPersonAddedToCollection += OnListenerAdded;
+		PersonManager.instance.SignalPersonRemovedFromCollection += OnListenerRemoved;
+		GameManager.instance.SignalPlayingStateEnter += OnPlayingStateEnter;
+	}
+
+	private void OnDisable() {
+		if (PersonManager.instance != null) {
+			PersonManager.instance.SignalPersonAddedToCollection -= OnListenerAdded;
+			PersonManager.instance.SignalPersonRemovedFromCollection -= OnListenerRemoved;
+		}
+		if (GameManager.instance != null) {
+			GameManager.instance.SignalPlayingStateEnter -= OnPlayingStateEnter;
+		}
+	}
+
+	private void OnPlayingStateEnter() {
+		UpdateText();
+	}
+
+	private void OnListenerAdded() {
+		UpdateText();
+	}
+
+	private void OnListenerRemoved() {
+		UpdateText();
+
+	}
+
+	private void UpdateText() {
+		text.text = GetString();
+	}
+
+	private string GetString() {
+		return GameManager.instance.droppedListeners.ToString("N0");
+	}
+}
--- Project/Assets/Scripts/UI/GameOverListenersText.cs
+++ Project/Assets/Scripts/UI/GameOverListenersText.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class GameOverListenersText : MonoBehaviour {
+public class GameOverInfoText : MonoBehaviour {
 	[SerializeField] private TextMeshProUGUI text;
+	private string infoString;
+	private string venueName;
+	private string venueLocation;
 
 	private void OnEnable() {
 		PersonManager.instance.SignalPersonAddedToCollection += OnListenerAdded;
@@ -40,6 +43,42 @@ public class GameOverListenersText : MonoBehaviour {
 	}
 
 	private string GetString() {
-		return GameManager.instance.droppedListeners.ToString("N0");
+
+		if (GameManager.instance.droppedListeners == 0) {
+			infoString = "That's not enough to fill anything. Yikes.";
+		} else if (GameManager.instance.droppedListeners <= 600) {
+			venueName = "the Music Hall of Williamsburg";
+			venueLocation = "New York, New York";
+		} else if (GameManager.instance.droppedListeners <= 1000) {
+			venueName = "the Great American Music Hall";
+			venueLocation = "San Francisco, California";
+		} else if (GameManager.instance.droppedListeners <= 2000) {
+			venueName = "the Paramount theatre";
+			venueLocation = "Denver, Colorado";
+		} else if (GameManager.instance.droppedListeners <= 2500) {
+			venueName = "Webster Hall";
+			venueLocation = "New York, New York";
+		} else if (GameManager.instance.droppedListeners <= 5000) {
+			venueName = "Saratoga Performing Arts Center";
+			venueLocation = "Saratoga Springs, New York";
+		} else if (GameManager.instance.droppedListeners <= 10000) {
+			venueName = "Ford Idaho Center";
+			venueLocation = "Nampa, Idaho";
+		} else if (GameManager.instance.droppedListeners <= 20000) {
+			venueName = "Madison Square Garden";
+			venueLocation = "New York, New York";
+		} else if (GameManager.instance.droppedListeners <= 40000) {
+			venueName = "Angel Stadium";
+			venueLocation = "Anaheim, California";
+		} else if (GameManager.instance.droppedListeners > 40000) {
+			venueName = "Levi's Stadium";
+			venueLocation = "Santa Clara, California";
+		}
+
+		if (GameManager.instance.droppedListeners != 0) {
+			infoString = "That's enough to fill " + venueName + " in " + venueLocation + ".";
+		}
+
+		return infoString;
 	}
 }
