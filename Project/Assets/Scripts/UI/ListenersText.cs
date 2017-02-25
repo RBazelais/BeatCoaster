20b0e8cf4afc0446521b908c9e31fc0379dcc86a
Whitaker Trebella <wtrebella@gmail.com>
2017-02-24 20:59:08 -0700

    - listeners text in (ACTUALLY HOPEFULLY)

---
+++ Project/Assets/Scripts/UI/ListenersText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ListenersText : MonoBehaviour {
+	[SerializeField] private TextMeshProUGUI text;
+
+	private void OnEnable() {
+		PersonManager.instance.SignalPersonAddedToCollection += OnListenerAdded;
+		PersonManager.instance.SignalPersonRemovedFromCollection += OnListenerRemoved;
+		GameManager.instance.SignalPlayingStateEnter += OnPlayingStateEnter;
+	}
+
+	private void OnDisable() {
+		if (PersonManager.instance != null) {
+			PersonManager.instance.SignalPersonAddedToCollection -= OnListenerAdded;
+			PersonManager.instance.SignalPersonRemovedFromCollection -= OnListenerRemoved;
+		}
+		if (GameManager.instance != null) {
+			GameManager.instance.SignalPlayingStateEnter -= OnPlayingStateEnter;
+		}
+	}
+
+	private void OnPlayingStateEnter() {
+		UpdateText();
+	}
+
+	private void OnListenerAdded() {
+		UpdateText();
+	}
+
+	private void OnListenerRemoved() {
+		UpdateText();
+
+	}
+
+	private void UpdateText() {
+		text.text = GetString();
+	}
+
+	private string GetString() {
+		return PersonManager.instance.GetListenerCount().ToString("N0") + " listeners";
+	}
+}
