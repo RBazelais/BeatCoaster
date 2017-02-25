ca7dd36724c28f181cc94eebd46f44af23a276d4
Brice Puls <briceepuls@gmail.com>
2017-02-25 12:07:35 -0700

    Implement button press to pick up

--- Project/Assets/Scripts/Player/Player_Collider.cs
+++ Project/Assets/Scripts/Player/Player_Collider.cs
@@ -3,41 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class Player_Collider : MonoBehaviour {
+public class Player_Collider : MonoBehaviour
+{
 
 	[SerializeField]
-	private BoxCollider2D _col;
+	private CircleCollider2D _col;
 
 	[SerializeField] private ParticleSystem collectParticles;
 
 	[SerializeField] private AudioClip[] hits;
 
+	[SerializeField] private SpriteRenderer _sprite;
+
 	private float beatPunchIntensity = 0.5f;
 
-	private void Start() {
+	private Enemy _currentEnemy;
+
+	private void Start ()
+	{
 		AudioManager.instance.BeatOnUpdate += OnBeat;
 	}
 
-	private void OnBeat() {
+	private void OnBeat ()
+	{
 		float dur = 60f / (float)AudioManager.instance.bpm - 0.1f;
-		transform.DOPunchScale(new Vector3(beatPunchIntensity, beatPunchIntensity, beatPunchIntensity), dur);
+		transform.DOPunchScale (new Vector3 (beatPunchIntensity, beatPunchIntensity, beatPunchIntensity), dur);
 	}
 
-	public void OnEnemyCollected(Enemy enemy) {
+	public void OnEnemyCollected (Enemy enemy)
+	{
 		var mainModule = collectParticles.main;
-		Color c = ColorManager.GetColorForTrackType(enemy.trail.trackType);
+		Color c = ColorManager.GetColorForTrackType (enemy.trail.trackType);
 		c.a = 0.5f;
 		mainModule.startColor = c;
-		collectParticles.Play();
-		AudioClip hit = hits[(int)enemy.trail.trackType];
-		AudioManager.instance.PlaySound(hit, 0.5f);
+		collectParticles.Play ();
+		AudioClip hit = hits [(int)enemy.trail.trackType];
+		AudioManager.instance.PlaySound (hit, 0.5f);
 	}
 
-	void OnTriggerEnter2D(Collider2D col)
+	void Update ()
 	{
-		var enemy = col.GetComponent<Enemy>();
-		if(!enemy.collected){
-			enemy.AttemptCollect();
+		if (_currentEnemy != null) {
+			if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.RightShift)) {
+				if (!_currentEnemy.collected) {
+					_currentEnemy.AttemptCollect ();
+				}
+			}
 		}
 	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		
+		_currentEnemy = col.GetComponent<Enemy> ();
+		_sprite.color = ColorManager.GetColorForTrackType (_currentEnemy.trackType);
+		_sprite.color = new Color (_sprite.color.r, _sprite.color.g, _sprite.color.b, .5f);
+	}
+
+	void OnTriggerExit2D (Collider2D col)
+	{
+		_sprite.color = new Color (1, 1, 1, .5f);
+		_currentEnemy = null;
+	}
+
+
 }
